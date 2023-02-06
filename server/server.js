f8e5b4fe7dfc5294a69e919da369c91366ac540a
Rustam <30387667+RustamZakAs@users.noreply.github.com>
2023-02-06 19:39:00 +0400

    localhost

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const mysql = require("mysql");
 app.use(cors());
 
 const con = mysql.createConnection({
-  host: "65.21.63.164",
+  host: "localhost",
   port: 3306,
   user: "root",
   password: "comtekbm",
